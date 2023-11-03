65b338f0a6c5cdf8e4639107c03ca108faab4e57
Matt Fergoda <mattfergoda@users.noreply.github.com>
2023-11-03 10:29:36 -0700

    Added tests.

--- productiv/src/EditableTodo.test.js
+++ productiv/src/EditableTodo.test.js
@@ -8,11 +8,13 @@ describe("EditableTodo component", function () {
         render(<EditableTodo todo={TEST_TODO} />);
     });
 
-    it("contains title, description, priority if not editing", function () {
+    it("contains todo, edit button, delete button if not editing", function () {
         const result = render(<EditableTodo todo={TEST_TODO} />);
         expect(result.queryByText(TEST_TODO.title)).toBeInTheDocument();
         expect(result.queryByText(TEST_TODO.description)).toBeInTheDocument();
         expect(result.queryByText(TEST_TODO.priority)).toBeInTheDocument();
+        expect(result.queryByText("Edit")).toBeInTheDocument();
+        expect(result.queryByText("Del")).toBeInTheDocument();
     });
 
     it("contains editing form if editing", function () {
@@ -22,6 +24,41 @@ describe("EditableTodo component", function () {
         fireEvent.click(button);
 
         expect(container.getElementsByClassName('NewTodoForm').length).toEqual(1);
+    });
+
+    it("calls submit function when submitting after editing", function () {
+        const update = jest.fn();
+        // update.mockReturnValue()
+
+        const { queryByText, container } = render(
+            <EditableTodo
+                todo={TEST_TODO}
+                update={update} />
+        );
+
+        const editButton = queryByText('Edit');
+        fireEvent.click(editButton);
+
+        const submitButton = queryByText('Gø!');
+        fireEvent.click(submitButton);
+
+        expect(update).toHaveBeenCalledTimes(1);
+    });
+
+    it("calls remove function when delete button pressed", function () {
+        const remove = jest.fn();
+        // update.mockReturnValue()
+
+        const { queryByText, container } = render(
+            <EditableTodo
+                todo={TEST_TODO}
+                remove={remove} />
+        )
+
+        const deleteButton = queryByText('Del');
+        fireEvent.click(deleteButton);
+
+        expect(remove).toHaveBeenCalledTimes(1);
     });
 
     it("matches snapshot", function () {
--- productiv/src/TodoApp.test.js
+++ productiv/src/TodoApp.test.js
@@ -3,6 +3,10 @@ import TodoApp from "./TodoApp";
 import { TEST_TODOS } from "./_commonTest";
 
 describe("TodoApp Tests", function () {
+    it("renders without crashing", function () {
+        render(<TodoApp initialTodos={TEST_TODOS} />);
+      });
+
     it("can add new todo", function () {
         const result = render(<TodoApp initialTodos={TEST_TODOS} />);
 
@@ -18,9 +22,13 @@ describe("TodoApp Tests", function () {
         fireEvent.change(description, { target: { value: "test-todo-description" } });
         fireEvent.change(priority, { target: { value: 1 } });
         fireEvent.click(button);
-        result.debug;
+
         expect(result.queryByText("test-todo")).toBeInTheDocument();
         expect(result.queryByText("test-todo-description")).toBeInTheDocument();
-        expect(result.queryByText("1")).toBeInTheDocument();
     });
+
+    it("matches snapshot", function () {
+        const { container } = render(<TodoApp initialTodos={TEST_TODOS} />);
+        expect(container).toMatchSnapshot();
+      });
 });
