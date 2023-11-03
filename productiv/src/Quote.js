f5b0ca5b18ee292b5aa79bf544059a2833642820
Logan Winnie <loganwinnier@gmail.com>
2023-11-03 19:35:08 -0400

    merged

--- productiv/src/Quote.js
+++ productiv/src/Quote.js
@@ -7,36 +7,10 @@
  * RandomInspoQuote -> Quote
  */
 
-<<<<<<< HEAD
 function Quote({ quote }) {
   return (<p className="Quote">
     <i>{quote.text} - {quote.author}</i>
   </p>);
-=======
-function Quote() {
-
-  const [showRandomQuote, setShowRandomQuote] = useState(false);
-
-  function handleGetQuote() {
-    setShowRandomQuote(true);
-  }
-
-  return (
-    <div className="Quote">
-      {
-        showRandomQuote
-          ?
-          <div>
-            <RandomInspoQuote />
-          </div>
-          :
-          <form onSubmit={handleGetQuote}>
-            <button>Get Inspirational Quote</button>
-          </form>
-      }
-    </div >
-  );
->>>>>>> 068b0c302dd76d3151f62f25b63462e9ad0e018a
 }
 
 export default Quote;
